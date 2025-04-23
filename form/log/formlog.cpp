8f1a98bfc6b6cb70d962cb3a921edb273f03e73f
MRsoymilk <codermrsoymilk@gmail.com>
2025-04-23 09:57:36 +0800

    update form log

--- form/log/formlog.cpp
+++ form/log/formlog.cpp
@@ -1,4 +1,6 @@
 #include "formlog.h"
+#include "UILogSink.h"
+#include "mylog.h"
 #include "ui_formlog.h"
 
 FormLog::FormLog(QWidget *parent)
@@ -6,6 +8,10 @@ FormLog::FormLog(QWidget *parent)
     , ui(new Ui::FormLog)
 {
     ui->setupUi(this);
+    auto ui_sink = std::make_shared<UILogSink_mt>(ui->txtLog);
+    ui_sink->set_pattern("[%H:%M:%S] [%l] %v");
+
+    MY_LOG.getLogger()->sinks().push_back(ui_sink);
 }
 
 FormLog::~FormLog()
