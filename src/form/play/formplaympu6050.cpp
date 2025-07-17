28089f276c1d78483f16c568131e58e183523161
MRsoymilk <codermrsoymilk@gmail.com>
2025-07-17 14:32:02 +0800

    remove Qt3D

--- src/form/play/formplaympu6050.cpp
+++ src/form/play/formplaympu6050.cpp
@@ -1,11 +1,12 @@
 #include "formplaympu6050.h"
 #include "ui_formplaympu6050.h"
 
-#include <QResizeEvent>
+#include <QFile>
 
 FormPlayMPU6050::FormPlayMPU6050(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::FormPlayMPU6050)
+    , m_model(nullptr)
 {
     ui->setupUi(this);
 
@@ -13,33 +14,6 @@ FormPlayMPU6050::FormPlayMPU6050(QWidget *parent)
     m_model->setHorizontalHeaderLabels({"X", "Y", "Z"});
     m_model->setVerticalHeaderLabels({"Accel", "Gyro", "Euler", "Temp"});
     ui->tableView->setModel(m_model);
-
-    m_view = new Qt3DExtras::Qt3DWindow();
-    m_container = QWidget::createWindowContainer(m_view);
-    ui->gLayPlane->addWidget(m_container);
-
-    m_rootEntity = new Qt3DCore::QEntity();
-
-    Qt3DRender::QCamera *camera = m_view->camera();
-    camera->lens()->setPerspectiveProjection(45.0f, 4.0f / 3.0f, 0.1f, 1000.0f);
-    camera->setPosition(QVector3D(0, 0, 10.0f));
-    camera->setViewCenter(QVector3D(0, 0, 0));
-
-    auto *camController = new Qt3DExtras::QOrbitCameraController(m_rootEntity);
-    camController->setCamera(camera);
-
-    auto *loader = new Qt3DRender::QSceneLoader(m_rootEntity);
-    loader->setSource(QUrl::fromLocalFile(":/res/models/airplane.gltf"));
-
-    m_airplaneTransform = new Qt3DCore::QTransform();
-    m_airplaneEntity = new Qt3DCore::QEntity(m_rootEntity);
-    m_airplaneEntity->addComponent(loader);
-    m_airplaneEntity->addComponent(m_airplaneTransform);
-
-    auto *material = new Qt3DExtras::QPhongMaterial(m_rootEntity);
-    m_airplaneEntity->addComponent(material);
-
-    m_view->setRootEntity(m_rootEntity);
 }
 
 FormPlayMPU6050::~FormPlayMPU6050()
@@ -68,9 +42,6 @@ void FormPlayMPU6050::onRecvMPU(const QByteArray &data)
             float yaw = parts[8].toFloat();
             float temp = parts[9].toFloat();
 
-            QQuaternion q = QQuaternion::fromEulerAngles(pitch, yaw, roll);
-            m_airplaneTransform->setRotation(q);
-
             m_model->setItem(0, 0, new QStandardItem(QString::number(ax, 'f', 2)));
             m_model->setItem(0, 1, new QStandardItem(QString::number(ay, 'f', 2)));
             m_model->setItem(0, 2, new QStandardItem(QString::number(az, 'f', 2)));
@@ -88,27 +59,4 @@ void FormPlayMPU6050::onRecvMPU(const QByteArray &data)
             m_model->setItem(3, 2, new QStandardItem(""));
         }
     }
-}
-
-void FormPlayMPU6050::resizeEvent(QResizeEvent *event)
-{
-    QWidget::resizeEvent(event);
-
-    if (!m_view || !m_airplaneTransform)
-        return;
-
-    QSize size = event->size();
-    float aspect = float(size.width()) / size.height();
-    auto *camera = m_view->camera();
-
-    camera->lens()->setPerspectiveProjection(45.0f, aspect, 0.1f, 1000.0f);
-
-    float baseDistance = 10.0f;
-    float scaleFactor = qMin(size.width(), size.height()) / 400.0f;
-    float camDistance = baseDistance / scaleFactor;
-    camera->setPosition(QVector3D(0, 0, camDistance));
-
-    float modelBaseScale = 1.0f;
-    float modelScale = scaleFactor * modelBaseScale;
-    m_airplaneTransform->setScale(modelScale);
 }
--- src/form/play/formplaympu6050.h
+++ src/form/play/formplaympu6050.h
@@ -3,14 +3,6 @@
 
 #include <QStandardItemModel>
 #include <QWidget>
-#include <Qt3DCore/QEntity>
-#include <Qt3DCore/QTransform>
-#include <Qt3DExtras/QForwardRenderer>
-#include <Qt3DExtras/QOrbitCameraController>
-#include <Qt3DExtras/QPhongMaterial>
-#include <Qt3DExtras/Qt3DWindow>
-#include <Qt3DRender/QCamera>
-#include <Qt3DRender/QSceneLoader>
 
 namespace Ui {
 class FormPlayMPU6050;
@@ -26,18 +18,8 @@ public:
 public slots:
     void onRecvMPU(const QByteArray &data);
 
-protected:
-    void resizeEvent(QResizeEvent *event) override;
-
 private:
     Ui::FormPlayMPU6050 *ui;
-
-    Qt3DExtras::Qt3DWindow *m_view;
-    QWidget *m_container;
-    Qt3DCore::QEntity *m_rootEntity;
-    Qt3DCore::QEntity *m_airplaneEntity;
-    Qt3DCore::QTransform *m_airplaneTransform;
-
     QStandardItemModel *m_model;
 };
 
