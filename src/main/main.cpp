dbda6896a2911423b13c63201b4f2e8dd9970fd3
MRsoymilk <codermrsoymilk@gmail.com>
2025-08-12 15:16:41 +0800

    single application

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSharedMemory>
 #include "funcdef.h"
 #include "version.h"
 
@@ -8,6 +9,16 @@ int main(int argc, char *argv[])
 {
     LOG_INFO("version: {}", APP_VERSION);
     QApplication a(argc, argv);
+
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("MySerial_soymilk");
+
+    if (!sharedMemory.create(1)) {
+        QMessageBox::warning(nullptr, "Warning", QObject::tr("Application is already running!"));
+
+        return 0;
+    }
+
     a.setWindowIcon(QIcon(":/res/icons/icon.png"));
     MainWindow w;
     w.show();
