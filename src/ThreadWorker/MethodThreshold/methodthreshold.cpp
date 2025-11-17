c31e3bd403f6503eb31e3c15134c69e70c5d9fb5
MRsoymilk <codermrsoymilk@gmail.com>
2025-11-17 11:11:38 +0800

    rm MethodThreshold

--- src/ThreadWorker/MethodThreshold/methodthreshold.cpp
+++
@@ -1,7 +0,0 @@
-#include "methodthreshold.h"
-
-MethodThreshold::MethodThreshold() {}
-
-void MethodThreshold::setParams(const QJsonObject &params) {}
-
-void MethodThreshold::generateThreshold() {}
