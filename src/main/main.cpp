9f852475947b33cab24c2ceca347911400f1c50d
MRsoymilk <codermrsoymilk@gmail.com>
2026-03-18 15:02:39 +0800

    add cli mode control

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QSharedMemory>
 
 #include "funcdef.h"
@@ -7,19 +9,44 @@
 
 int main(int argc, char *argv[]) {
     LOG_INFO("version: {}", APP_VERSION);
+
     QApplication a(argc, argv);
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("MySerial");
+    parser.addHelpOption();
+
+    QCommandLineOption option_mode("mode", "Run mode: easy | produce | expert", "mode");
+
+    parser.addOption(option_mode);
+    parser.process(a);
+
+    QString mode = parser.value(option_mode).toLower();
+
+    if (mode.isEmpty()) {
+        mode = "expert";
+    }
+
+    if (mode != "easy" && mode != "produce" && mode != "expert") {
+        QMessageBox::warning(nullptr, TITLE_WARNING, QObject::tr("Invalid mode! Use easy/produce/expert"));
+        LOG_WARN("Invalid mode: {}", mode);
+        return 0;
+    }
+
     QSharedMemory sharedMemory;
-    sharedMemory.setKey("MySerial_soymilk");
+    sharedMemory.setKey("MySerial_soymilk_" + mode);
 
     if (!sharedMemory.create(1)) {
         QMessageBox::warning(nullptr, TITLE_WARNING, QObject::tr("Application is already running!"));
-
+        LOG_WARN("Application is already running!");
         return 0;
     }
 
     a.setWindowIcon(QIcon(":/res/icons/icon.png"));
+
     MainWindow w;
+    w.setMode(mode);
     w.show();
+
     return a.exec();
 }
